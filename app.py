e5b3d5378fae2f896848aac45a97902a9603976a
Mohammed Saif Ali <44836109+mohammedsaifali@users.noreply.github.com>
2024-01-20 16:08:16 +0400

    Update app.py

--- app.py
+++ app.py
@@ -26,11 +26,12 @@ def prepare_data(file_path, include_rate=True) -> pd.DataFrame:
 
 def to_excel(df):
     output = BytesIO()
-    writer = pd.ExcelWriter(output, engine='xlsxwriter')
-    df.to_excel(writer, index=False, sheet_name='Sheet1')
-    writer.save()
+    with pd.ExcelWriter(output, engine='xlsxwriter') as writer:
+        df.to_excel(writer, index=False, sheet_name='Sheet1')
+        writer.save()
     processed_data = output.getvalue()
     return processed_data
+
 
 def main():
     st.header("Emaar Analytics :bar_chart:")
